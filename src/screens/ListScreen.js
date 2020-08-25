82639a374a65a61c13a2629ac4543c4579497295
joshdlcruz <dcjoshua25@gmail.com>
2020-08-26 00:42:30 +0800

    updated screens

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,20 +1,46 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  StatusBar,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-ionicons';
 
-function ListScreen() {
+function ListScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>List Screen</Text>
+      <View style={styles.fabContainer}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Modal')}
+          style={styles.fabButton}>
+          <Icon ios="ios-add" android="md-add" color="#fff" size={70} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
+  },
+  fabContainer: {
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    position: 'absolute',
+    right: 10,
+    bottom: 20,
+  },
+  fabButton: {
+    backgroundColor: 'blue',
+    borderRadius: 35,
+    width: 70,
+    height: 70,
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
-
 export default ListScreen;
--- src/screens/ModalScreen.js
+++ src/screens/ModalScreen.js
@@ -1,10 +1,68 @@
+/* eslint-disable no-undef */
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  TextInput,
+} from 'react-native';
+import Icon from 'react-native-ionicons';
 
-function ModalScreen() {
+function ModalScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Modal Screen</Text>
+      <View style={styles.innerContainer}>
+        <View style={styles.closeButtonContainer}>
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => navigation.goBack()}>
+            <Icon
+              ios="ios-close"
+              android="md-close"
+              color="#101010"
+              size={40}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.modalContainer}>
+          <Text style={{color: '#444', fontSize: 20}}>
+            What do you want to do?
+          </Text>
+          <TextInput
+            style={{
+              height: 50,
+              width: 200,
+              padding: 5,
+              borderColor: 'gray',
+              borderBottomWidth: 1,
+            }}
+            numberOfLines={1}
+            onChangeText={(value) => setValue(value)}
+            value={value}
+            clearButtonMode="while-editing"
+          />
+          <TouchableOpacity
+            style={{
+              marginTop: 10,
+              backgroundColor: 'blue',
+              width: 50,
+              height: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: 5,
+            }}
+            onPress={() => navigation.navigate('List')}>
+            <Icon
+              ios="ios-arrow-dropright-circle"
+              android="md--arrow-dropright-circle"
+              size={40}
+              color="#fff"
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,9 +70,43 @@ function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  innerContainer: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    height: '30%',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
     backgroundColor: '#fff',
+  },
+
+  closeButtonContainer: {
+    position: 'absolute',
+    alignItems: 'flex-end',
+    right: 10,
+  },
+
+  closeButton: {
+    backgroundColor: '#d3d3d3',
+    borderRadius: 20,
+    width: 40,
+    height: 40,
+    top: 10,
     alignItems: 'center',
+
     justifyContent: 'center',
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    margin: 60,
+    top: 10,
+    left: 50,
   },
 });
 
